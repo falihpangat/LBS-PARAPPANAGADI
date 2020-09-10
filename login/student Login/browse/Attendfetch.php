50a0a871a7969e19ed5ab24728411f0234fab706
FALIH PANGAT <59413538+falihpangat@users.noreply.github.com>
2020-09-10 15:11:24 +0530

    Add files via upload

---
+++ login/student Login/browse/Attendfetch.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>table with database</title>
+	<style type="text/css">
+	table {
+		border-collapse: collapse;
+		width: 100%;
+		color: #d96459;
+		font-family: monospace;
+		font-size: 25px;
+		text-align: left;
+	}
+	th{
+		background-color: #d96459;
+		color: white;
+	}
+	tr:nth-child(even) {background-color: #f2f2f2;}
+	
+	</style>
+</head>
+<body>
+	<h1 align="center"><strong>ATTANDENCE TABLE</strong></h1>
+	<table>
+		<tr>
+			<th>REGISTER</th>
+			<th>Student name</th>
+			<th>Course</th>
+			<th>Semester</th>
+			<th>Percentege</th>
+		</tr>
+		<?php
+		$conn = mysqli_connect("localhost","root","","lbscollege");
+		if ($conn-> connect_error) {
+			die("connection failed:". $conn-> connect_error);
+		}
+
+		$sql = "SELECT id, name,course,sem,perc from attandence";
+		$result = $conn-> query($sql);
+
+		if ($result-> num_rows > 0) {
+		while ($row = $result-> fetch_assoc()) {
+        echo "<tr>
+        	<td>". $row["id"] ."</td>
+        	<td>". $row["name"] ."</td>
+        	<td>". $row["course"] ."</td>
+        	<td>".$row["sem"] ."</td>
+        	<td>".$row["perc"] ."</td>
+            </tr>.";
+			}
+			echo "</table>";
+		}
+		else {
+			echo "0 result";
+		}
+
+		$conn-> close();
+		?>
+	</table>
+
+</body>
+</html>
